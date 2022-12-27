d4c0ca9027268974733705043b02bf9040aec76c
codebydolapo <bashorun115@gmail.com>
2022-12-27 03:50:32 -0800

    fixed blog cards

--- components/BlogItem.tsx
+++ components/BlogItem.tsx
@@ -6,7 +6,7 @@ function BlogItem({ image, title, desc, link }: { image: string, title: string, 
     return (
         <div className={`min-h-[35vw] h-auto md:w-[48vw] flex flex-col rounded-lg my-[2rem] xs:w-[98vw] relative ${styles.blogitem}`}>
             <div className={`w-full min-h-[4rem] flex justify-start items-start pl-2`}>
-                <h1 className={`font-extrabold md:text-xl capitalize xs:text-sm`}>{title}</h1>
+                <h1 className={`font-extrabold md:text-xl capitalize xs:text-lg`}>{title}</h1>
             </div>
             <Link href={link} passHref= {true}>
                 <div className={`w-full md:min-h-[20rem] h-auto flex justify-center relative rounded-xl mb-2 overflow-hidden object-contain xs:min-h-[50vw]`}>
