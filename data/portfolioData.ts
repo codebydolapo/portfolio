2fff2e1a5bff16973d11f49672414b412380bb8e
unknown <Bashorun Dolapo>
2023-09-09 16:46:11 +0100

    Chore: Portfolio restart

--- data/portfolioData.ts
+++ data/portfolioData.ts
@@ -1,37 +1,44 @@
 export const portfolioData = [
+    // {
+    //     title: 'Enefti: NFT Marketplace',
+    //     desc: "Nft Marketplace: Shop, find a jpeg you like, own it",
+    //     images: ['images/enefti/enefti1.png', 'images/enefti/enefti2.png', 'images/enefti/enefti3.png', 'images/enefti/enefti4.png'],
+    //     liveLink: 'https://eneftii.netlify.app',
+    //     repoLink: 'https://github.com/codebydolapo/Enefti'
+    // },
+    // {
+    //     title: 'Purple: Web3 Store',
+    //     desc: "Buy yourself a pair of jeans and headphones with ether",
+    //     images: ['images/purple/purple1.png', 'images/purple/purple2.png', 'images/purple/purple3.png', 'images/purple/purple4.png'],
+    //     liveLink: 'https://purplecollections.netlify.app',
+    //     repoLink: 'https://github.com/codebydolapo/Purple-Collections'
+    // },
+    // {
+    //     title: 'Solaris: Space Tourism Venture',
+    //     desc: "A convenient and easy way to go to space",
+    //     images: ['images/solaris/solaris.png', 'images/solaris/solaris2.png'],
+    //     liveLink: 'https://solariis.netlify.app',
+    //     repoLink: 'https://github.com/codebydolapo/Solaris'
+    // },
+    // {
+    //     title: 'Sequin: Blockchain Hotel App',
+    //     desc: "Hotel Rental App On The Matic Blockchain",
+    //     images: ['images/sequin/sequinRevamped.png','images/sequin/sequin.png', 'images/sequin/sequinRentals.png'],
+    //     liveLink: '',
+    //     repoLink: 'https://github.com/codebydolapo/sequin/'
+    // },
+    // {
+    //     title: 'GelatoSwap: Crypto Exchange',
+    //     desc: "Crypto Exchange With Native Cryptocurrency",
+    //     images: ['images/gelatoswap/gelatoswap2.png', 'images/gelatoswap/gelatoswap3.png', 'images/gelatoswap/gelatoswap.png'],
+    //     liveLink: '',
+    //     repoLink: 'https://github.com/codebydolapo/GelatoSwap'
+    // },
     {
-        title: 'Enefti: NFT Marketplace',
-        desc: "Nft Marketplace: Shop, find a jpeg you like, own it",
-        images: ['images/enefti/enefti1.png', 'images/enefti/enefti2.png', 'images/enefti/enefti3.png', 'images/enefti/enefti4.png'],
-        liveLink: 'https://eneftii.netlify.app',
-        repoLink: 'https://github.com/codebydolapo/Enefti'
-    },
-    {
-        title: 'Purple: Web3 Store',
-        desc: "Buy yourself a pair of jeans and headphones with ether",
-        images: ['images/purple/purple1.png', 'images/purple/purple2.png', 'images/purple/purple3.png', 'images/purple/purple4.png'],
-        liveLink: 'https://purplecollections.netlify.app',
-        repoLink: 'https://github.com/codebydolapo/Purple-Collections'
-    },
-    {
-        title: 'Solaris: Space Tourism Venture',
-        desc: "A convenient and easy way to go to space",
-        images: ['images/solaris/solaris.png', 'images/solaris/solaris2.png'],
-        liveLink: 'https://solariis.netlify.app',
-        repoLink: 'https://github.com/codebydolapo/Solaris'
-    },
-    {
-        title: 'Sequin: Blockchain Hotel App',
-        desc: "Hotel Rental App On The Matic Blockchain",
-        images: ['images/sequin/sequinRevamped.png','images/sequin/sequin.png', 'images/sequin/sequinRentals.png'],
+        title: 'Coming Soon',
+        desc: "Postfolio under construction...",
+        images: ['https://camo.githubusercontent.com/43d150068e9cd7ce2e4870e01c1652afbf47eadd9e00bff08782a18c3cdbf218/68747470733a2f2f657468657265756d2e6f72672f7374617469632f32383231346262363865623534343564636230363361373235333562633930632f66353161332f6865726f2e706e67'],
         liveLink: '',
-        repoLink: 'https://github.com/codebydolapo/sequin/'
+        repoLink: 'https://github.com/codebydolapo'
     },
-    {
-        title: 'GelatoSwap: Crypto Exchange',
-        desc: "Crypto Exchange With Native Cryptocurrency",
-        images: ['images/gelatoswap/gelatoswap2.png', 'images/gelatoswap/gelatoswap3.png', 'images/gelatoswap/gelatoswap.png'],
-        liveLink: '',
-        repoLink: 'https://github.com/codebydolapo/GelatoSwap'
-    },
-]//#0078ff
+]
